97f5d2ae36b080d3dd7f3a67bc6b079c02353d44
oshkaStudy <oshkax2@gmail.com>
2025-06-06 19:36:39 +0300

    added demoqa_test task

--- src/test/java/tests/PageObjectTest.java
+++ src/test/java/tests/PageObjectTest.java
@@ -1,9 +1,11 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import pages.components.ResultModalComponent;
 
+@Tag("demoqa")
 public class PageObjectTest extends TestBase {
 
     //Вызов page-объектов
