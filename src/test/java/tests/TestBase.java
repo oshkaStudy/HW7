309a9566cb616d810a1ccc488f051a2d40454ea5
oshkaStudy <oshkax2@gmail.com>
2025-06-06 19:40:47 +0300

    added headless mode

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -11,6 +11,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.headless = true;
 
     }
 
